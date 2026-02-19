fc53ccfc4296370688211bb76f351d5b9b962493
Klauz España <kespana@bitacore.xyz>
2026-02-18 21:24:14 -0500

    Cambio de apellido

--- envs/qa/backend.tf
+++ envs/qa/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-cicd-terraform-app-apellido" # Reemplazar por apellido
-    storage_account_name = "tfstateapellido"                # Reemplazar por apellido
+    resource_group_name  = "rg-cicd-terraform-app-espana" # Reemplazar por espana
+    storage_account_name = "tfstateespana"                # Reemplazar por espana
     container_name       = "tfstate"
     key                  = "qa/terraform.tfstate"
   }
